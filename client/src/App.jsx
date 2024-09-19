8bc9f59862b17920a2021b63b04d729a35b5dc46
Patryk <p.szczerbinski2014gg@gmail.com>
2024-09-19 14:52:54 +0200

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="home" element={<Home />} />
-        <Route path="home" element={<Home />} />
+        <Route path="home" element={<Home />} /> 
         
       </Routes>
     </BrowserRouter>
